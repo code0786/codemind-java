1faecbe7e43cefdb97d624cdc884f5e9baf4ce68
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-05 19:01:57 +0530

    import java.util.*; class Love{ 	public static void main(String args[]){ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int[] arr = new int[n]; 		for(int i = 0 ; i < n ; i++){ 		    arr[i] = sc.nextInt(); 		} 		int count = 0; 		for(int i = 0 ; i < n ; i++){ 		    int c = 1 ; 		    for(int j = 0 ; j < n ; j++){ 		        if(i!=j && arr[j]!=0 && arr[i]!=0 && arr[i]==arr[j]){ 		            c++; 		            arr[j] = 0; 		        } 		    } 		    if(arr[i]!=0 && arr[i]==c){ 		        System.out.format("%d ",arr[i]); 		        count++; 		    } 		    arr[i] = 0 ; 		} 		if(count == 0){ 		    System.out.format("-1"); 		} 	} }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Love{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int count = 0;
+		for(int i = 0 ; i < n ; i++){
+		    int c = 1 ;
+		    for(int j = 0 ; j < n ; j++){
+		        if(i!=j && arr[j]!=0 && arr[i]!=0 && arr[i]==arr[j]){
+		            c++;
+		            arr[j] = 0;
+		        }
+		    }
+		    if(arr[i]!=0 && arr[i]==c){
+		        System.out.format("%d ",arr[i]);
+		        count++;
+		    }
+		    arr[i] = 0 ;
+		}
+		if(count == 0){
+		    System.out.format("-1");
+		}
+	}
+}
