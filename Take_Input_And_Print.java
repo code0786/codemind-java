5dc4ff5a3c92a82071cc212041af7d6284459e3b
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-05 18:34:14 +0530

    import java.util.Scanner;  class Kaif{     public static void main(String[] args){         Scanner k=new Scanner(System.in);         String saif=k.nextLine();         System.out.println(saif);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+ class Kaif{
+    public static void main(String[] args){
+        Scanner k=new Scanner(System.in);
+        String saif=k.nextLine();
+        System.out.println(saif);
+    }
+}
