452a498316a52b6d10b3239f6fe545c98cd72ce7
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-05 18:58:56 +0530

    import java.util.*; class Love{ 	public static void main(String args[]){ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int[] arr = new int[n]; 		for(int i = 0 ; i < n ; i++){ 		    arr[i] = sc.nextInt(); 		} 		int sum = 0 ; 		for(int i = 0 ; i < n ; i++){ 		    int c = 0 ; 		    for(int j = i ; j < n ; j++){ 		        if(arr[i]==arr[j]){ 		            c++; 		        } 		    } 		    if(c==1 && arr[i]%2==0){ 		        sum++; 		    } 		} 		System.out.format("%d",sum); 	} }

---
+++ unique_even_count.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Love{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int sum = 0 ;
+		for(int i = 0 ; i < n ; i++){
+		    int c = 0 ;
+		    for(int j = i ; j < n ; j++){
+		        if(arr[i]==arr[j]){
+		            c++;
+		        }
+		    }
+		    if(c==1 && arr[i]%2==0){
+		        sum++;
+		    }
+		}
+		System.out.format("%d",sum);
+	}
+}
