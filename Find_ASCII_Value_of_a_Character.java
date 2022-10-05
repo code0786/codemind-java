4974420656a56e0d4e881bf8b84a5bb3355b4448
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-05 18:28:02 +0530

    import java.util.*; class Love{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         char s = sc.next().charAt(0);         int c=s;         System.out.format("%d",c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Love{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        char s = sc.next().charAt(0);
+        int c=s;
+        System.out.format("%d",c);
+    }
+}
