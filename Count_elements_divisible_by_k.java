f6d23a5211faadc0f549656bcff98dcf528b9734
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-05 18:41:18 +0530

    import java.util.*; class Love{ 	public static void main(String args[]){ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int k = sc.nextInt(); 		int count = 0; 		int[] arr = new int[n]; 		for(int i = 0 ; i < n ; i++){ 		    arr[i] = sc.nextInt(); 		    if(arr[i]%k==0){ 		        count++; 		    } 		} 		System.out.format("%d",count); 	} }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Love{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int count = 0;
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++){
+		    arr[i] = sc.nextInt();
+		    if(arr[i]%k==0){
+		        count++;
+		    }
+		}
+		System.out.format("%d",count);
+	}
+}
