c800c24ff05db58f4d6ae1a52fec47fe37b18dc7
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-07 10:56:32 +0530

    import java.util.*; class Love{ 	public static void main(String args[]){ 		Scanner sc = new Scanner(System.in); 		int f = sc.nextInt(); 		double c = (f-32)*(5.0/9.0); 		System.out.format("%.2f",c); 	} }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Love{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int f = sc.nextInt();
+		double c = (f-32)*(5.0/9.0);
+		System.out.format("%.2f",c);
+	}
+}
