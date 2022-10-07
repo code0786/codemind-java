04c19e2828820666c8b7d2448ed21fe7abca8901
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-08 00:06:46 +0530

    import java.util.Scanner; class La{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int r=s.nextInt();         int res= r*(r-3)/2;         System.out.print(res);
        }
    }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class La{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int r=s.nextInt();
+        int res= r*(r-3)/2;
+        System.out.print(res);
+        
+    }
+}
